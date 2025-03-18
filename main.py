62d1ee5eddc9356eeb47afc6c01791a252f63a1f
helitito <heliogoncalves11@hotmail.com>
2025-03-18 19:35:04 -0300

    Adicionada foto de fundo

--- main.py
+++ main.py
@@ -2,14 +2,32 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+
+#Screen size
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+#Background
+BG = pygame.transform.scale(pygame.image.load('Background.jpeg'),(SCREEN_WIDTH,SCREEN_HEIGHT))
+def draw():
+    screen.blit(BG,(0,0))
+    pygame.display.update()
+#Clock variable
 clock = pygame.time.Clock()
+
+#Game running
 running = True
 dt = 0
+
 #GAME NAME
 pygame.display.set_caption('Are we out of the woods yet?')
 
-#Starting position
+#Surface
+test_surface = pygame.Surface((100,200))
+test_surface.fill('black')
+#PLAYER
+ #Starting position
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height()-40)
 
 #Game Loop
@@ -19,9 +37,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    #Draw the background
+    draw()
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("darkgreen")
 
     pygame.draw.circle(screen, "black", player_pos, 40)
 
@@ -35,13 +53,17 @@ while running:
         player_pos.x -= 300 * dt
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
+    if keys[pygame.K_i]:
+        pass
 
-    # flip() the display to put your work on screen
+
+# flip() the display to put your work on screen
     pygame.display.flip()
 
     # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
+    # dt is delta time in seconds since last frame, used for framerate
     # independent physics.
     dt = clock.tick(60) / 1000
 
 pygame.quit()
+
